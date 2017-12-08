507f04c363f6e0c2617c027a18166bea3632dea8
NikitaSmygin <30476122+NikitaSmygin@users.noreply.github.com>
2017-12-08 17:03:34 +0300

    AJAX

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+
+$recepient = "nikitasmygin@yandex.ru";
+$sitename = "Технологос";
+
+$name = trim($_POST["name"]);
+$phone = trim($_POST["phone"]);
+$email = trim($_POST["email"]);
+$text = trim($_POST["text"]);
+$message = "Имя: $name \nТелефон: $phone \nПочта: $email \nТекст: $text";
+
+$pagetitle = "Новая заявка с сайта \"$sitename\"";
+mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
